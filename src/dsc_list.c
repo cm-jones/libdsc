20fd6529de9a6798b80f5f859080b929cb0bd082
Cody Michael Jones <codymjones@pm.me>
2024-04-15 13:49:14 -0500

    Add GPL comment at head of each source file

--- src/dsc_list.c
+++ src/dsc_list.c
@@ -1,18 +1,19 @@
 /*
- * This file is part of Data Structures for C.
+ * This file is part of the Data Structures for C (DSC) library.
  *
- * Data Structures for C is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
+ * The Data Structures for C (DSC) library is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
- * Data Structures for C is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
+ * The Data Structures for C (DSC) library is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * Data Structures for C. If not, see <https://www.gnu.org/licenses/>.
+ * the Data Structures for C (DSC) library. If not, see
+ * <https://www.gnu.org/licenses/>.
  */
 
 #include <stdlib.h>
