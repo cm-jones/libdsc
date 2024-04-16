44d1edd2a51d9d83aceaa1c7c49b3014d1856e7b
Cody Michael Jones <codymjones@pm.me>
2024-04-16 10:29:06 -0500

    Update tests to reflect new error handling

--- tests/test_dsc_list.c
+++ tests/test_dsc_list.c
@@ -24,17 +24,6 @@ START_TEST(test_dsc_list_create)
     struct dsc_list_t *list = dsc_list_create();
     ck_assert_ptr_nonnull(list);
     ck_assert_ptr_null(list->head);
-    ck_assert_int_eq(list->error, DSC_ERROR_NONE);
-    dsc_list_destroy(list);
-}
-END_TEST
-
-START_TEST(test_dsc_list_create_with_error)
-{
-    struct dsc_list_t *list = dsc_list_create_with_error(DSC_ERROR_OUT_OF_MEMORY);
-    ck_assert_ptr_nonnull(list);
-    ck_assert_ptr_null(list->head);
-    ck_assert_int_eq(list->error, DSC_ERROR_OUT_OF_MEMORY);
     dsc_list_destroy(list);
 }
 END_TEST
@@ -42,18 +31,20 @@ END_TEST
 START_TEST(test_dsc_list_destroy)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_prepend(list, 1);
-    list = dsc_list_prepend(list, 2);
-    list = dsc_list_destroy(list);
-    ck_assert_ptr_null(list);
+    dsc_list_prepend(list, 1);
+    dsc_list_prepend(list, 2);
+    enum dsc_error_t error = dsc_list_destroy(list);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
 }
 END_TEST
 
 START_TEST(test_dsc_list_prepend)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_prepend(list, 1);
-    list = dsc_list_prepend(list, 2);
+    enum dsc_error_t error = dsc_list_prepend(list, 1);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
+    error = dsc_list_prepend(list, 2);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 2);
     ck_assert_int_eq(list->head->next->value, 1);
     dsc_list_destroy(list);
@@ -63,8 +54,10 @@ END_TEST
 START_TEST(test_dsc_list_append)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
+    enum dsc_error_t error = dsc_list_append(list, 1);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
+    error = dsc_list_append(list, 2);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 1);
     ck_assert_int_eq(list->head->next->value, 2);
     dsc_list_destroy(list);
@@ -74,13 +67,15 @@ END_TEST
 START_TEST(test_dsc_list_insert)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_insert(list, 1, 0);
-    list = dsc_list_insert(list, 2, 1);
-    list = dsc_list_insert(list, 3, 1);
+    enum dsc_error_t error = dsc_list_insert(list, 1, 0);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
+    error = dsc_list_insert(list, 2, 1);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
+    error = dsc_list_insert(list, 3, 1);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 1);
     ck_assert_int_eq(list->head->next->value, 3);
     ck_assert_int_eq(list->head->next->next->value, 2);
-
     dsc_list_destroy(list);
 }
 END_TEST
@@ -88,9 +83,10 @@ END_TEST
 START_TEST(test_dsc_list_insert_before)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_insert_before(list, list->head->next, 3);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    enum dsc_error_t error = dsc_list_insert_before(list, list->head->next, 3);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 1);
     ck_assert_int_eq(list->head->next->value, 3);
     ck_assert_int_eq(list->head->next->next->value, 2);
@@ -101,9 +97,10 @@ END_TEST
 START_TEST(test_dsc_list_insert_after)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_insert_after(list, list->head, 3);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    enum dsc_error_t error = dsc_list_insert_after(list, list->head, 3);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 1);
     ck_assert_int_eq(list->head->next->value, 3);
     ck_assert_int_eq(list->head->next->next->value, 2);
@@ -114,9 +111,10 @@ END_TEST
 START_TEST(test_dsc_list_delete_head)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_delete_head(list);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    enum dsc_error_t error = dsc_list_delete_head(list);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 2);
     ck_assert_ptr_null(list->head->next);
     dsc_list_destroy(list);
@@ -126,9 +124,10 @@ END_TEST
 START_TEST(test_dsc_list_delete_tail)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_delete_tail(list);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    enum dsc_error_t error = dsc_list_delete_tail(list);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 1);
     ck_assert_ptr_null(list->head->next);
     dsc_list_destroy(list);
@@ -138,10 +137,11 @@ END_TEST
 START_TEST(test_dsc_list_delete_at_position)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 3);
-    list = dsc_list_delete_at_position(list, 1);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 3);
+    enum dsc_error_t error = dsc_list_delete_at_position(list, 1);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 1);
     ck_assert_int_eq(list->head->next->value, 3);
     dsc_list_destroy(list);
@@ -151,11 +151,12 @@ END_TEST
 START_TEST(test_dsc_list_delete_value)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 3);
-    list = dsc_list_delete_value(list, 2);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 3);
+    enum dsc_error_t error = dsc_list_delete_value(list, 2);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 1);
     ck_assert_int_eq(list->head->next->value, 2);
     ck_assert_int_eq(list->head->next->next->value, 3);
@@ -166,11 +167,12 @@ END_TEST
 START_TEST(test_dsc_list_delete_all)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 3);
-    list = dsc_list_delete_all(list, 2);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 3);
+    enum dsc_error_t error = dsc_list_delete_all(list, 2);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 1);
     ck_assert_int_eq(list->head->next->value, 3);
     dsc_list_destroy(list);
@@ -180,10 +182,11 @@ END_TEST
 START_TEST(test_dsc_list_reverse)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 3);
-    list = dsc_list_reverse(list);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 3);
+    enum dsc_error_t error = dsc_list_reverse(list);
+    ck_assert_int_eq(error, DSC_ERROR_NONE);
     ck_assert_int_eq(list->head->value, 3);
     ck_assert_int_eq(list->head->next->value, 2);
     ck_assert_int_eq(list->head->next->next->value, 1);
@@ -194,9 +197,9 @@ END_TEST
 START_TEST(test_dsc_list_search)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 3);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 3);
     struct dsc_node_t *node = dsc_list_search(list, 2);
     ck_assert_ptr_nonnull(node);
     ck_assert_int_eq(node->value, 2);
@@ -207,10 +210,10 @@ END_TEST
 START_TEST(test_dsc_list_count)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 3);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 3);
     int count = dsc_list_count(list, 2);
     ck_assert_int_eq(count, 2);
     dsc_list_destroy(list);
@@ -221,7 +224,7 @@ START_TEST(test_dsc_list_is_empty)
 {
     struct dsc_list_t *list = dsc_list_create();
     ck_assert(dsc_list_is_empty(list));
-    list = dsc_list_append(list, 1);
+    dsc_list_append(list, 1);
     ck_assert(!dsc_list_is_empty(list));
     dsc_list_destroy(list);
 }
@@ -231,9 +234,9 @@ START_TEST(test_dsc_list_get_length)
 {
     struct dsc_list_t *list = dsc_list_create();
     ck_assert_int_eq(dsc_list_get_length(list), 0);
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 3);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 3);
     ck_assert_int_eq(dsc_list_get_length(list), 3);
     dsc_list_destroy(list);
 }
@@ -242,9 +245,9 @@ END_TEST
 START_TEST(test_dsc_list_get_nth_node)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
-    list = dsc_list_append(list, 3);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
+    dsc_list_append(list, 3);
     struct dsc_node_t *node = dsc_list_get_nth_node(list, 1);
     ck_assert_ptr_nonnull(node);
     ck_assert_int_eq(node->value, 2);
@@ -255,8 +258,8 @@ END_TEST
 START_TEST(test_dsc_list_get_head)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
     struct dsc_node_t *head = dsc_list_get_head(list);
     ck_assert_ptr_nonnull(head);
     ck_assert_int_eq(head->value, 1);
@@ -267,8 +270,8 @@ END_TEST
 START_TEST(test_dsc_list_get_tail)
 {
     struct dsc_list_t *list = dsc_list_create();
-    list = dsc_list_append(list, 1);
-    list = dsc_list_append(list, 2);
+    dsc_list_append(list, 1);
+    dsc_list_append(list, 2);
     struct dsc_node_t *tail = dsc_list_get_tail(list);
     ck_assert_ptr_nonnull(tail);
     ck_assert_int_eq(tail->value, 2);
@@ -285,7 +288,6 @@ Suite *dsc_list_suite(void)
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, test_dsc_list_create);
-    tcase_add_test(tc_core, test_dsc_list_create_with_error);
     tcase_add_test(tc_core, test_dsc_list_destroy);
     tcase_add_test(tc_core, test_dsc_list_prepend);
     tcase_add_test(tc_core, test_dsc_list_append);
