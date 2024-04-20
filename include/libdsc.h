4a5f633edc53fe97d290b9aa3202b6a3e14e4bd6
Cody Michael Jones <codymjones@pm.me>
2024-04-20 18:31:24 -0500

    Add libdsc.h

---
+++ include/libdsc.h
@@ -0,0 +1,28 @@
+/*
+ * This file is part of libdsc.
+ *
+ * libdsc is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * libdsc is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * libdsc. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIBDSC_H__
+#define __LIBDSC_H__
+
+#include "dsc_list.h"
+#include "dsc_vector.h"
+#include "dsc_stack.h"
+#include "dsc_queue.h"
+#include "dsc_map.h"
+#include "dsc_set.h"
+
+#endif /* __LIBDSC_H__ */
