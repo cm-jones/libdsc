73f2e7f4e937bc567c9d4a2d2676a8c313eeaaa8
Cody Michael Jones <codymjones@pm.me>
2024-04-27 00:01:19 -0500

    Change dsc_error() to dsc_error_get()

--- include/dsc_list.h
+++ include/dsc_list.h
@@ -156,6 +156,6 @@ bool dsc_list_erase(DSCList list, int position);
  *
  * @return The last error code.
  */
-DSCError dsc_error(const DSCList list);
+DSCError dsc_error_get(const DSCList list);
 
 #endif // __DSC_LIST_H__
