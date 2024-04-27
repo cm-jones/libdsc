fd056a96f771650adb3bf281854d01802fc84e16
Cody Michael Jones <codymjones@pm.me>
2024-04-27 11:18:44 -0500

    Finish implementing generic queue container and using a circular buffer

--- include/dsc_queue.h
+++ include/dsc_queue.h
@@ -21,67 +21,43 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#include "dsc_type.h"
+#include "dsc_error.h"
+
 /**
  * @def DSC_QUEUE_INITIAL_CAPACITY
  * @brief The initial capacity of the queue.
  */
 #define DSC_QUEUE_INITIAL_CAPACITY 16
 
-// Forward declaration of the queue structure
+/* Forward declaration of the queue structure */
 typedef struct DSCQueue *DSCQueue;
+
+/* Constructor and destructor for a DSCQueue */
 
 /**
  * @brief Creates a new empty queue.
  *
+ * @param type The type of the elements in the queue.
+ *
  * @return A pointer to the newly created queue, or NULL if memory allocation fails.
  */
-DSCQueue dsc_queue_init(void);
+DSCQueue dsc_queue_init(DSCType type);
 
 /**
  * @brief Destroys the queue and frees its memory.
  *
  * @param queue The queue to destroy.
- * 
+ *
  * @return true if the queue was destroyed successfully, false otherwise.
+ *
+ * @note This function frees the memory allocated for the queue and its elements.
+ *       It is the responsibility of the caller to ensure that any dynamically
+ *       allocated elements are properly freed before calling this function.
  */
 bool dsc_queue_deinit(DSCQueue queue);
 
-/**
- * @brief Inserts an element at the end of the queue.
- *
- * @param queue The queue to push the element into.
- * @param value The value to push.
- * 
- * @return true if the element was inserted successfully, false otherwise.
- */
-bool dsc_queue_push(DSCQueue queue, int value);
-
-/**
- * @brief Removes the element at the front of the queue.
- *
- * @param queue The queue to pop from.
- * 
- * @return The popped value, or -1 if the queue is NULL.
- */
-int dsc_queue_pop(DSCQueue queue);
-
-/**
- * @brief Retrieves the value of the element at the front of the queue without removing it.
- *
- * @param queue The queue to get the front element from.
- *
- * @return The value of the front element, or -1 on failure.
- */
-int dsc_queue_front(const DSCQueue queue);
-
-/**
- * @brief Retrieves the value of the element at the back of the queue without removing it.
- *
- * @param queue The queue to get the back element from.
- *
- * @return The value of the back element, or -1 on failure.
- */
-int dsc_queue_back(const DSCQueue queue);
+/* Capacity */
 
 /**
  * @brief Checks if the queue is empty.
@@ -97,8 +73,62 @@ bool dsc_queue_is_empty(const DSCQueue queue);
  *
  * @param queue The queue to get the size of.
  *
- * @return The number of elements in the queue, -1 if the queue is NULL.
+ * @return The number of elements in the queue, or -1 if the queue is NULL.
  */
 int dsc_queue_size(const DSCQueue queue);
+
+/* Element access */
+
+/**
+ * @brief Retrieves the value of the element at the front of the queue without removing it.
+ *
+ * @param queue The queue to get the front element from.
+ *
+ * @return The value of the front element, or NULL if the queue is empty or NULL.
+ */
+void *dsc_queue_front(const DSCQueue queue);
+
+/**
+ * @brief Retrieves the value of the element at the back of the queue without removing it.
+ *
+ * @param queue The queue to get the back element from.
+ *
+ * @return The value of the back element, or NULL if the queue is empty or NULL.
+ */
+void *dsc_queue_back(const DSCQueue queue);
+
+/* Modifiers */
+
+/**
+ * @brief Inserts an element at the end of the queue.
+ *
+ * @param queue The queue to push the element into.
+ * @param data The value to push.
+ *
+ * @return true if the element was inserted successfully, false otherwise.
+ *
+ * @note The type of the element must match the type specified during queue initialization.
+ */
+bool dsc_queue_push(DSCQueue queue, void *data);
+
+/**
+ * @brief Removes the element at the front of the queue.
+ *
+ * @param queue The queue to pop from.
+ *
+ * @return The popped value, or NULL if the queue is empty or NULL.
+ *
+ * @note The caller is responsible for freeing the memory of the popped element if necessary.
+ */
+void *dsc_queue_pop(DSCQueue queue);
+
+/**
+ * @brief Retrieves the last error code.
+ *
+ * @param queue The queue to get the error code from.
+ *
+ * @return The last error code.
+ */
+DSCError dsc_error_get(const DSCQueue queue);
 
 #endif // __DSC_QUEUE_H__
--- src/dsc_queue.c
+++ src/dsc_queue.c
@@ -20,154 +20,154 @@
 #include <stdint.h>
 
 #include "../include/dsc_queue.h"
-#include "../include/dsc_error.h"
+
+/* Represents a queue */
 
 struct DSCQueue {
-    int *values;  // Array of values stored in the queue
-    int size;     // The number of elements in the queue
-    int capacity; // The current capacity of the queue
+    void **data;     /* Array of data stored in the queue */
+    size_t front;    /* Index of the front element */
+    size_t rear;     /* Index of the rear element */
+    size_t size;     /* The number of elements currently in the queue */
+    size_t capacity; /* The current capacity of the queue */
+    DSCType type;    /* The type of the elements in the queue */
+    DSCError error;  /* The most recent error code */
 };
 
-static bool dsc_queue_resize(DSCQueue queue, int new_capacity) {
-    int *new_values = realloc(queue->values, new_capacity * sizeof(int));
-    if (new_values == NULL) {
-        dsc_set_error(DSC_ERROR_OUT_OF_MEMORY);
+/* Helper function for resizing a DSCQueue when its size is equal to or greater than its capacity */
+
+static bool dsc_queue_resize(DSCQueue queue, size_t new_capacity) {
+    void **new_data = realloc(queue->data, new_capacity * dsc_size_of(queue->type));
+    if (!new_data) {
         return false;
     }
 
-    queue->values = new_values;
-    queue->capacity = new_capacity;
+    // Rearrange elements in case of circular buffer
+    if (queue->front > queue->rear) {
+        size_t count = queue->capacity - queue->front;
+        memmove(new_data + new_capacity - count, queue->data + queue->front, count * dsc_size_of(queue->type));
+        queue->front = new_capacity - count;
+    }
 
-    dsc_set_error(DSC_ERROR_NONE);
+    queue->data = new_data;
+    queue->capacity = new_capacity;
     return true;
 }
 
-DSCQueue dsc_queue_init(void) {
-    DSCQueue new_queue = malloc(sizeof *new_queue);
-    if (new_queue == NULL) {
-        dsc_set_error(DSC_ERROR_OUT_OF_MEMORY);
+DSCQueue dsc_queue_init(DSCType type) {
+    // Check whether type is a valid DSCType
+    if (type < 0 || type >= DSC_TYPE_COUNT) {
         return NULL;
     }
 
+    DSCQueue new_queue = malloc(sizeof *new_queue);
+    if (!new_queue) {
+        return NULL;
+    }
+
+    new_queue->front = 0;
+    new_queue->rear = 0;
     new_queue->size = 0;
     new_queue->capacity = DSC_QUEUE_INITIAL_CAPACITY;
-    
-    new_queue->values = malloc(new_queue->capacity * sizeof(int));
-    if (new_queue->values == NULL) {
+    new_queue->type = type;
+
+    new_queue->data = malloc(new_queue->capacity * dsc_size_of(type));
+    if (!new_queue->data) {
         free(new_queue);
-        
-        dsc_set_error(DSC_ERROR_OUT_OF_MEMORY);    
         return NULL;
     }
 
-    dsc_set_error(DSC_ERROR_NONE);
     return new_queue;
 }
 
-void dsc_queue_free(DSCQueue queue) {
-    if (queue == NULL) {
-        dsc_set_error(DSC_ERROR_INVALID_ARGUMENT);
-        return;
-    }
-
-    free(queue->values);
-    free(queue);
-
-    dsc_set_error(DSC_ERROR_NONE);
-}
-
-bool dsc_queue_push(DSCQueue queue, int value) {
-    if (queue == NULL) {
-        dsc_set_error(DSC_ERROR_INVALID_ARGUMENT);
+bool dsc_queue_deinit(DSCQueue queue) {
+    if (!queue) {
         return false;
     }
 
-    /* Resize the queue if the size exceeds the capacity. */
-    if (queue->size >= queue->capacity) {
-        unsigned int new_capacity = queue->capacity * 1.5;
-        if (!dsc_queue_resize(queue, new_capacity)) {
-            dsc_set_error(DSC_ERROR_OUT_OF_MEMORY);
-            return false;
-        }
+    // Free the elements in the queue
+    for (size_t i = queue->front; i != queue->rear; i = (i + 1) % queue->capacity) {
+        free(queue->data[i]);
     }
 
-    queue->values[queue->size] = value;
-    queue->size++;
-
-    dsc_set_error(DSC_ERROR_NONE);
+    free(queue->data);
+    free(queue);
     return true;
 }
 
-int dsc_queue_pop(DSCQueue queue) {
-    if (queue == NULL) {
-        dsc_set_error(DSC_ERROR_INVALID_ARGUMENT);
-        return -1;
-    }
-
-    if (dsc_queue_is_empty(queue)) {
-        dsc_set_error(DSC_ERROR_EMPTY_CONTAINER);
-        return -1;
-    }
-
-    int result = queue->values[0];
-
-    // Shift elements to the left
-    for (int i = 0; i < queue->size - 1; ++i) {
-        queue->values[i] = queue->values[i + 1];
-    }
-
-    queue->size--;
-
-    dsc_set_error(DSC_ERROR_NONE);
-    return result;
-}
-
-int dsc_queue_front(const DSCQueue queue) {
-    if (queue == NULL) {
-        dsc_set_error(DSC_ERROR_INVALID_ARGUMENT);
-        return -1;
-    }
-
-    if (dsc_queue_is_empty(queue)) {
-        dsc_set_error(DSC_ERROR_EMPTY_CONTAINER);
-        return -1;
-    }
-
-    dsc_set_error(DSC_ERROR_NONE);
-    return queue->values[0];
-}
-
-int dsc_queue_back(const DSCQueue queue) {
-    if (queue == NULL) {
-        dsc_set_error(DSC_ERROR_INVALID_ARGUMENT);
-        return -1;
-    }
-
-    if (dsc_queue_empty(queue)) {
-        dsc_set_error(DSC_ERROR_EMPTY_CONTAINER);
-        return -1;
-    }
-
-    dsc_set_error(DSC_ERROR_NONE);
-    return queue->values[queue->size - 1];
-}
+/* Capacity */
 
 bool dsc_queue_is_empty(const DSCQueue queue) {
-    if (queue == NULL) {
-        dsc_set_error(DSC_ERROR_INVALID_ARGUMENT);
+    if (!queue) {
         return true;
     }
 
-    dsc_set_error(DSC_ERROR_NONE);
     return queue->size == 0;
 }
 
 int dsc_queue_size(const DSCQueue queue) {
-    if (queue == NULL) {
-        dsc_set_error(DSC_ERROR_INVALID_ARGUMENT);
+    if (!queue) {
         return -1;
     }
 
-    dsc_set_error(DSC_ERROR_NONE);
     return queue->size;
+}
+
+/* Element access */
+
+void *dsc_queue_front(const DSCQueue queue) {
+    if (!queue || dsc_queue_is_empty(queue)) {
+        return NULL;
+    }
+
+    return queue->data[queue->front];
+}
+
+void *dsc_queue_back(const DSCQueue queue) {
+    if (!queue || dsc_queue_is_empty(queue)) {
+        return NULL;
+    }
+
+    return queue->data[(queue->rear - 1 + queue->capacity) % queue->capacity];
+}
+
+/* Modifiers */
+
+bool dsc_queue_push(DSCQueue queue, void *data) {
+    if (!queue) {
+        return false;
+    }
+
+    // Check if the type of the new element matches the type of the queue
+    if (dsc_type_of(data) != queue->type) {
+        queue->error = DSC_ERROR_TYPE_MISMATCH;
+        return false;
+    }
+
+    // Resize the queue if the size reaches the capacity
+    if (queue->size == queue->capacity) {
+        size_t new_capacity = queue->capacity * 2;
+        if (!dsc_queue_resize(queue, new_capacity)) {
+            return false;
+        }
+    }
+
+    queue->data[queue->rear] = data;
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->size++;
+    return true;
+}
+
+void *dsc_queue_pop(DSCQueue queue) {
+    if (!queue || dsc_queue_is_empty(queue)) {
+        return NULL;
+    }
+
+    void *data = queue->data[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size--;
+    return data;
+}
+
+DSCError dsc_error_get(const DSCQueue queue) {
+    return queue->error;
 }
