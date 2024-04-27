812e574a3945620dd1cbd17ceba4631a83568088
Cody Michael Jones <codymjones@pm.me>
2024-04-26 23:59:22 -0500

    Document dsc_error.h

--- include/dsc_error.h
+++ include/dsc_error.h
@@ -22,40 +22,56 @@
  * @enum DSCError
  * @brief Represents the error codes used in the DSC library.
  *
- * @constant DSC_ERROR_NONE                       No error occurred.
- * @constant DSC_ERROR_INVALID_ARGUMENT           An invalid argument was passed to a function.
- * @constant DSC_ERROR_OUT_OF_RANGE               The index is out of bounds.
- * @constant DSC_ERROR_OUT_OF_MEMORY              Failed to allocate memory.
- * @constant DSC_ERROR_EMPTY_CONTAINER            The container is empty.
- * @constant DSC_ERROR_KEY_NOT_FOUND              The key was not found in the associative container.
- * @constant DSC_ERROR_VECTOR_CAPACITY_EXCEEDED   The vector capacity has been exceeded.
- * @constant DSC_ERROR_HASHMAP_KEY_ALREADY_EXISTS The key already exists in the hash map.
- * @constant DSC_ERROR_QUEUE_FULL                 The queue is full.
+ * The DSCError enum defines a set of error codes that are used to indicate
+ * the success or failure of operations in the DSC library. These error codes
+ * provide a way to identify and handle specific error conditions that may
+ * occur while using the library.
+ *
+ * The error codes are returned by DSC library functions to indicate the
+ * success or failure of an operation. When a function encounters an error,
+ * it sets the error code using the appropriate error constant from the
+ * DSCError enum and returns an appropriate value (e.g., NULL, false, or an
+ * invalid value) to indicate the failure. The user can then check the return
+ * value and handle the error accordingly.
+ *
+ * It is the responsibility of the user to check the return values of the
+ * library functions and handle the errors appropriately based on their
+ * specific use case and requirements.
+ *
+ * @constant DSC_ERROR_OK
+ *           No error occurred. The operation was successful.
+ *
+ * @constant DSC_ERROR_TYPE_MISMATCH
+ *           There was a mismatch between the expected type and the actual type
+ *           of the data.
+ *
+ * @constant DSC_ERROR_INVALID_ARGUMENT
+ *           An invalid argument was passed to a function.
+ *
+ * @constant DSC_ERROR_OUT_OF_RANGE
+ *           The specified index or position is out of the valid range.
+ *
+ * @constant DSC_ERROR_OUT_OF_MEMORY
+ *           Memory allocation failed.
+ *
+ * @constant DSC_ERROR_EMPTY_CONTAINER
+ *           The container is empty.
+ *
+ * @constant DSC_ERROR_KEY_NOT_FOUND
+ *           The specified key was not found in the container.
+ *
+ * @constant DSC_ERROR_HASHMAP_KEY_ALREADY_EXISTS
+ *           The specified key already exists in the hashmap.
  */
 typedef enum DSCError {
-    DSC_ERROR_NONE,
+    DSC_ERROR_OK,
+    DSC_ERROR_TYPE_MISMATCH,
     DSC_ERROR_INVALID_ARGUMENT,
     DSC_ERROR_OUT_OF_RANGE,
     DSC_ERROR_OUT_OF_MEMORY,
     DSC_ERROR_EMPTY_CONTAINER,
     DSC_ERROR_KEY_NOT_FOUND,
-    DSC_ERROR_VECTOR_CAPACITY_EXCEEDED,
-    DSC_ERROR_HASHMAP_KEY_ALREADY_EXISTS,
-    DSC_ERROR_QUEUE_FULL
+    DSC_ERROR_HASHMAP_KEY_ALREADY_EXISTS
 } DSCError;
-
-/**
- * @brief Sets the last error code.
- *
- * @param error The error code to set.
- */
-void dsc_set_error(DSCError error);
-
-/**
- * @brief Gets the last error code.
- *
- * @return The last error code.
- */
-DSCError dsc_get_error(void);
 
 #endif // __DSC_ERROR_H__
