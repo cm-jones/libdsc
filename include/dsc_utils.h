4507348800cb2939286e2a486937702fd97f2618
Cody Michael Jones <codymjones@pm.me>
2024-05-24 07:18:09 -0500

    Remove dsc_compare()

--- include/dsc_utils.h
+++ include/dsc_utils.h
@@ -15,45 +15,15 @@
  * libdsc. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef __DSC_UTILS_H__
-#define __DSC_UTILS_H__
+#ifndef DSC_UTILS
+#define DSC_UTILS
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #include "dsc_type.h"
+#include "dsc_error.h"
 
-/**
- * @brief Compares two values of the specified type.
- *
- * This function compares two values of the specified type and returns an integer
- * indicating their relationship. The function supports comparison of various
- * data types defined in the DSCType enum.
- *
- * @param a Pointer to the first value.
- * @param b Pointer to the second value.
- * @param type The DSCType value representing the type of the values being compared.
- * @return An integer indicating the relationship between the values:
- *         - A negative value if a < b
- *         - Zero if a == b
- *         - A positive value if a > b
- *
- * @note The behavior is undefined if the pointers a and b do not point to valid
- *       values of the specified type. It is the caller's responsibility to ensure
- *       that the pointers are valid and the type matches the actual data type.
- */
-int dsc_compare(void *a, void *b, DSCType type);
-
-/**
- * @brief Calculates the hash value for a given key and capacity.
- *
- * This function uses a simple modulo-based hash function to calculate the hash
- * value for a given key and capacity.
- *
- * @param key The key to calculate the hash value for.
- * @param capacity The capacity of the hash set or map.
- * @return The calculated hash value.
- */
 uint32_t dsc_hash(void *key, DSCType key_type, size_t capacity);
 
-#endif // __DSC_UTILS_H__
+#endif // DSC_UTILS
