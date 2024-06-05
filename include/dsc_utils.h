fa248afc131c1abd03b558308cabe4427148040b
Cody Michael Jones <46515483+cm-jones@users.noreply.github.com>
2024-06-04 22:56:18 -0500

    Fix formatting

--- include/dsc_utils.h
+++ include/dsc_utils.h
@@ -21,9 +21,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "dsc_type.h"
 #include "dsc_error.h"
+#include "dsc_type.h"
 
 uint32_t dsc_hash(void *key, DSCType key_type, size_t capacity);
 
-#endif // DSC_UTILS
+#endif  // DSC_UTILS
